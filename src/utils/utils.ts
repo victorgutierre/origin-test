9335a42707b0b61cadcb600fe90e1060e272f128
victorgutierre <victorsgutierre@gmail.com>
2021-04-22 22:08:41 -0300

    change date file to utils

---
+++ src/utils/utils.ts
@@ -0,0 +1,23 @@
+export const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+
+export const money = (str: string) => {
+  if (!str) return '';
+
+  return parseFloat(str, 10)
+    .toFixed(2)
+    .replace(/(\d)(?=(\d{3})+\.)/g, '$1.')
+    .replace(/.(\d{2})$/g, ',$1');
+}
